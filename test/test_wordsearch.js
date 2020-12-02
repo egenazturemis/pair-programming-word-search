ca8888898880e14ceacc1010b570da5be760d6c6
Egenaz <egenazturemis@gmail.com>
2020-12-02 22:20:17 +0000

    implemented horizontal, vertical, horizontal backwards, and vertical backwards word search over a 2d matrix

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,64 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("should return true if the word is present - its present backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'DLEFNIES')
+
+    assert.isTrue(result);
+  });
+
+
+//
+it("should return true if the word is present", function() {
+  const result = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'ASYHW')
+
+  assert.isTrue(result);
+});
+it("should return: letters matrix is not of type: Array", function() {
+  const result = wordSearch({}, 'ASYHW');
+
+  assert.strictEqual(result, "letters matrix is not of type: Array."); 
+});
+
+it("should return: letters matrix is empty", function() {
+  const result = wordSearch([], 'ASYHW')
+
+  assert.strictEqual(result, "letters matrix is empty.");
+});
+
+it("should return true if the word is present vertically backwards", function() {
+  const result = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'EOUBW')
+
+  assert.isTrue(result);
+});
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,61 @@
 const wordSearch = (letters, word) => { 
+    if (letters.length === 0) {
+        return "letters matrix is empty."
+    }
+
+    if (Array.isArray(letters) === false){
+        return "letters matrix is not of type: Array."
+    }
+
+    // normal horizontal search 
     const horizontalJoin = letters.map(ls => ls.join(''))
+
+  
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    // normal vertical search 
+    const lettersTransposed = transpose(letters);
+    const verticalJoin = lettersTransposed.map(ls => ls.join(''))
+  
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    // backwards horizontal search 
+      let lettersBackwardsH = letters.map(ls => ls.reverse());
+      const horizontalJoinBackwards = lettersBackwardsH.map(ls => ls.join(''));
+
+      for (l of horizontalJoinBackwards) {
+        if (l.includes(word)) return true;
+    }
+     // backwards vertical search 
+     const lettersTransposed2 = transpose(letters);
+     const lettersTransposedBackwards = lettersTransposed2.map(ls => ls.reverse());
+     const verticalJoinBackwards = lettersTransposedBackwards.map(ls => ls.join(''));
+   
+     for (l of verticalJoinBackwards) {
+         if (l.includes(word)) return true;
+     }
+
+
+    return false;
 }
 
 module.exports = wordSearch
+
+
+function transpose(matrix){
+    let newArray = [];
+    for(let i = 0; i < matrix[0].length; i++){
+        newArray.push([]);
+    };
+  
+    for(let i = 0; i < matrix[0].length; i++){
+        for(let j = 0; j < matrix.length; j++){
+            newArray[i].push(matrix[j][i]);
+        };
+    };
+  
+    return newArray;
+  }
